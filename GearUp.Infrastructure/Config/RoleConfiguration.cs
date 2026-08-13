5c2a63c2c4c9415e2a3fa636e983a2c1138af824
Sherlock <playyala@yahoo.com>
2026-08-13 21:39:47 +0300

    add roles

---
+++ GearUp.Infrastructure/Config/RoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GearUp.Infrastructure.Config
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole { Id = "admin-id", Name = "Admin", NormalizedName = "ADMIN", ConcurrencyStamp = "admin-concurrency-stamp" },
+                new IdentityRole { Id = "customer-id", Name = "Customer", NormalizedName = "CUSTOMER", ConcurrencyStamp = "customer-concurrency-stamp" },
+                new IdentityRole { Id = "vendor-id", Name = "Vendor", NormalizedName = "VENDOR", ConcurrencyStamp = "vendor-concurrency-stamp" }
+
+            );
+        }
+    }
+}
---
+++ GearUp.Infrastructure/Migrations/20260813183911_AddRoles.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GearUp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "admin-id", "admin-concurrency-stamp", "Admin", "ADMIN" },
+                    { "customer-id", "customer-concurrency-stamp", "Customer", "CUSTOMER" },
+                    { "vendor-id", "vendor-concurrency-stamp", "Vendor", "VENDOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "customer-id");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "vendor-id");
+        }
+    }
+}
