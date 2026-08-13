41f9d61ef911d1e98743988182c6c7123ae9fc8f
Sherlock <playyala@yahoo.com>
2026-08-13 18:49:01 +0300

    add get all product endpoint

---
+++ GearUp.API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using GearUp.Core.Entities;
+using GearUp.Core.Enum;
+using GearUp.Core.Interfaces;
+using GearUp.Core.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GearUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(IUnitOfWork unit) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery] ProductSpecParams productParams)
+        {
+            // Force status to be Approved for public shop
+            productParams.Status = ProductStatus.Approved;
+
+            var spec = new ProductSpecification(productParams);
+
+            var result = await CreatePagedResult(unit.Repository<Product>(), spec,
+                productParams.PageIndex, productParams.PageSize);
+
+            return result;
+        }
+    }
+}
