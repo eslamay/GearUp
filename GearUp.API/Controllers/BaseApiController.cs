34f73bbf712cbc179aa23ac245cabbc27cc6ccda
Sherlock <playyala@yahoo.com>
2026-08-13 18:32:56 +0300

    add baseApiController & pagination

---
+++ GearUp.API/Controllers/BaseApiController.cs
@@ -0,0 +1,37 @@
+﻿using GearUp.API.RequestHelpers;
+using GearUp.Core.Entities;
+using GearUp.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GearUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        protected async Task<ActionResult> CreatePagedResult<T>(IGenericRepository<T> repository,
+            ISpecification<T> specification, int pageIndex, int pageSize) where T : BaseEntity
+        {
+            var items = await repository.ListAsync(specification);
+            var totalItems = await repository.CountAsync(specification);
+            var pagination = new Pagination<T>(pageIndex, pageSize, totalItems, items);
+            return Ok(pagination);
+        }
+
+        protected async Task<ActionResult> CreatePagedResult<T, TDto>(IGenericRepository<T> repo,
+            ISpecification<T> spec, int pageIndex, int pageSize, Func<T, TDto> toDto)
+                where T : BaseEntity, IDtoConvertible
+                where TDto : class
+        {
+            var items = await repo.ListAsync(spec);
+            var count = await repo.CountAsync(spec);
+
+            var dtoItems = items.Select(toDto).ToList();
+
+            var pagination = new Pagination<TDto>(pageIndex, pageSize, count, dtoItems);
+
+            return Ok(pagination);
+        }
+    }
+}
