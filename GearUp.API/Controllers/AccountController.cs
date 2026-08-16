2f76509bd894768c441752598021bc3bbe877235
Sherlock <playyala@yahoo.com>
2026-08-16 16:46:03 +0300

    add register endpoint

---
+++ GearUp.API/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using GearUp.API.DTOs;
+using GearUp.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GearUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(SignInManager<AppUser> signInManager) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var allowedRoles = new[] { "Customer", "Vendor" };
+            if (!allowedRoles.Contains(registerDto.Role))
+            {
+                return BadRequest("Invalid role selected");
+            }
+
+            var user = new AppUser
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                UserName = registerDto.Username
+            };
+
+            var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return ValidationProblem();
+            }
+
+            await signInManager.UserManager.AddToRoleAsync(user, registerDto.Role);
+
+            return Ok();
+        }
+    }
+}
