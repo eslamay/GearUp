4a8420a15ce4751513ac88774b01c432247c79dd
Sherlock <playyala@yahoo.com>
2026-08-16 16:48:18 +0300

    add login endpoint

--- GearUp.API/Controllers/AccountController.cs
+++ GearUp.API/Controllers/AccountController.cs
@@ -42,5 +42,29 @@ namespace GearUp.API.Controllers
 
             return Ok();
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginDto loginDto)
+        {
+            var user = await signInManager.UserManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null)
+            {
+                user = await signInManager.UserManager.FindByNameAsync(loginDto.Email);
+            }
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid email/username or password");
+            }
+            var result = await signInManager.PasswordSignInAsync(user, loginDto.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "Login successful" });
+            }
+
+            return Unauthorized("Invalid email/username or password");
+        }
     }
 }
