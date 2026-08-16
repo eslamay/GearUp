7e157f9cacdfc744bdba7c169444540c91be98d7
Sherlock <playyala@yahoo.com>
2026-08-16 16:33:54 +0300

    add login,register & address dto

---
+++ GearUp.API/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GearUp.API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required] 
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string Role { get; set; } = string.Empty;
+        [Required]
+        public string Username { get; set; } = string.Empty;
+    }
+}
