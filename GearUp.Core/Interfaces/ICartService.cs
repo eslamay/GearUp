2700c018d0666c2782c20eebaf45c1d22960fbe6
Sherlock <playyala@yahoo.com>
2026-08-17 20:55:59 +0300

    add cart service

---
+++ GearUp.Core/Interfaces/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using GearUp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GearUp.Core.Interfaces
+{
+    public interface ICartService
+    {
+        Task<ShoppingCart?> GetCartAsync(string cartId);
+        Task<ShoppingCart?> SetCartAsync(ShoppingCart cart);
+        Task<bool> DeleteCartAsync(string cartId);
+    }
+}
---
+++ GearUp.Infrastructure/Services/CartService.cs
@@ -0,0 +1,35 @@
+﻿using GearUp.Core.Entities;
+using GearUp.Core.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace GearUp.Infrastructure.Services
+{
+    public class CartService(IConnectionMultiplexer redis) : ICartService
+    {
+        private readonly IDatabase _database = redis.GetDatabase();
+        
+        public async Task<ShoppingCart?> GetCartAsync(string cartId)
+        {
+            var data = await _database.StringGetAsync(cartId);
+
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<ShoppingCart>(data.ToString());
+        }
+
+        public async Task<ShoppingCart?> SetCartAsync(ShoppingCart cart)
+        {
+            var created = await _database.StringSetAsync(cart.Id,
+            JsonSerializer.Serialize(cart), TimeSpan.FromDays(30));
+
+            if (!created) return null;
+
+            return await GetCartAsync(cart.Id);
+        }
+
+        public async Task<bool> DeleteCartAsync(string cartId)
+        {
+            return await _database.KeyDeleteAsync(cartId);
+        }
+    }
+}
