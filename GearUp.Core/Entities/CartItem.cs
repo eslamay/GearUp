745f58cefeaf7ca7a270665d8f9e9b1d024cd2b8
Sherlock <playyala@yahoo.com>
2026-08-17 20:32:47 +0300

    add CartItem + ShoppingCart Entities

---
+++ GearUp.Core/Entities/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GearUp.Core.Entities
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+    }
+}
