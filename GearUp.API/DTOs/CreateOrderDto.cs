4569a68450b5bf6661b7d7563e491aba2e802038
Sherlock <playyala@yahoo.com>
2026-08-18 20:46:08 +0300

    add order dto, order mapping extentions

---
+++ GearUp.API/DTOs/CreateOrderDto.cs
@@ -0,0 +1,21 @@
+﻿using GearUp.Core.Entities.OrderAggregate;
+using System.ComponentModel.DataAnnotations;
+
+namespace GearUp.API.DTOs
+{
+    public class CreateOrderDto
+    {
+        [Required]
+        public string CartId { get; set; } = string.Empty;
+
+        [Required]
+        public int DeliveryMethodId { get; set; }
+
+        [Required]
+        public ShippingAddress ShippingAddress { get; set; } = null!;
+
+        [Required]
+        public PaymentSummary PaymentSummary { get; set; } = null!;
+        public decimal Discount { get; set; }
+    }
+}
---
+++ GearUp.API/DTOs/OrderItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GearUp.API.DTOs
+{
+    public class OrderItemDto
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; } = string.Empty;
+        [Required]
+        public string PictureUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
