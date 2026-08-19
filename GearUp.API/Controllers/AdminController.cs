ce3f58401f29b2b8a24e14bfced0b5867532f646
Sherlock <playyala@yahoo.com>
2026-08-19 20:11:58 +0300

    add get orders by admin

---
+++ GearUp.API/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using GearUp.API.DTOs;
+using GearUp.API.Extensions;
+using GearUp.Core.Entities;
+using GearUp.Core.Entities.OrderAggregate;
+using GearUp.Core.Interfaces;
+using GearUp.Core.Specifications;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GearUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController(IUnitOfWork unit, IPaymentService paymentService, UserManager<AppUser> userManager) : BaseApiController
+    {
+        [HttpGet("orders")]
+        public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrders([FromQuery] OrderSpecParams specParams)
+        {
+            var spec = new OrderSpecification(specParams);
+
+            return await CreatePagedResult(unit.Repository<Order>(),
+                spec, specParams.PageIndex, specParams.PageSize, o => o.ToDto());
+        }
+
+        [HttpGet("orders/{id:int}")]
+        public async Task<ActionResult<OrderDto>> GetOrderById(int id)
+        {
+            var spec = new OrderSpecification(id);
+
+            var order = await unit.Repository<Order>().GetEntityWithSpec(spec);
+
+            if (order == null) return BadRequest("No order with that Id");
+
+            return order.ToDto();
+        }
+    }
+}
