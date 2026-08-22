a4e6a068ff1c5a3578726bf958885493ce5a4267
Sherlock <playyala@yahoo.com>
2026-08-22 17:47:47 +0300

    Add Interceptors

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://localhost:7250/api/',
+  hubUrl: 'https://localhost:7250/hub/notifications',
+  stripePublicKey: 'pk_test_51S9fNF3vym2Oyut0tDTVicNMEQMNCNkWIgMUYGzBxpT0YJwQAnGEVIuzmTl1ahEkXfd38Bm5XgHpiQ7mb0FmLbUF00M5hOZZcs'
+};
