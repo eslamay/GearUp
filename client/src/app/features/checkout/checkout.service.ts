18cb70aaa0266259931ada4a3901ff9327452f9e
Sherlock <playyala@yahoo.com>
2026-08-23 23:43:52 +0300

    add checkout service

---
+++ client/src/app/features/checkout/checkout.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DeliveryMethod } from '../../core/models/delivery-method';
+import { CreateOrderDto } from '../../core/models/create-order-dto';
+import { OrderDto } from '../../core/models/order';
+import { ShoppingCart } from '../../core/models/shopping-cart';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+  private baseUrl = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  getDeliveryMethods(): Observable<DeliveryMethod[]> {
+    return this.http.get<DeliveryMethod[]>(`${this.baseUrl}/Orders/delivery-methods`);
+  }
+
+  createOrder(orderDto: CreateOrderDto): Observable<OrderDto> {
+    return this.http.post<OrderDto>(`${this.baseUrl}/Orders`, orderDto);
+  }
+
+  createOrUpdatePaymentIntent(cartId: string): Observable<ShoppingCart> {
+    return this.http.post<ShoppingCart>(`${this.baseUrl}/Payments/${cartId}`, {});
+  }
+}
