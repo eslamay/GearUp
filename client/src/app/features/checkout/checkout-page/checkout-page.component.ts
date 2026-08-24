184be5240d8c35052a35aad6586f9c6df82425f6
Sherlock <playyala@yahoo.com>
2026-08-24 21:18:05 +0300

    add address step

--- client/src/app/features/checkout/checkout-page/checkout-page.component.ts
+++ client/src/app/features/checkout/checkout-page/checkout-page.component.ts
@@ -1,13 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { SpinnerComponent } from '../../../shared/components/spinner/spinner.component';
 import { Router, RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 import { CartService } from '../../cart/cart.service';
 import { CheckoutService } from '../checkout.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AccountService } from '../../account/account.service';
 
 @Component({
   selector: 'app-checkout-page',
-  imports: [CurrencyPipe, RouterLink, SpinnerComponent],
+  imports: [CurrencyPipe, RouterLink,ReactiveFormsModule, SpinnerComponent],
   templateUrl: './checkout-page.component.html',
   styleUrl: './checkout-page.component.css'
 })
@@ -15,8 +17,25 @@ export class CheckoutPageComponent implements OnInit {
   cartService = inject(CartService);
   private checkoutService = inject(CheckoutService);
   private router = inject(Router);
+  private accountService = inject(AccountService);
+  private fb = inject(FormBuilder);
 
+  @ViewChild('paymentElement') paymentElementRef?: ElementRef<HTMLDivElement>;
+  
   loading = true;
+  currentStep = 1;
+
+  // address form
+  addressForm = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    line1: ['', Validators.required],
+    line2: [''],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    postalCode: ['', Validators.required],
+    country: ['', Validators.required]
+  });
+  savingAddress = false;
 
   ngOnInit() {
     this.cartService.getCart().subscribe({
@@ -30,6 +49,30 @@ export class CheckoutPageComponent implements OnInit {
       error: () => {
         this.loading = false;
         this.router.navigateByUrl('/cart');
+      }
+    });
+  }
+
+
+  // address form methods
+  prefillAddress() {
+    const address = this.accountService.currentUser()?.address;
+    if (address) {
+      this.addressForm.patchValue(address);
+    }
+  }
+
+  saveAddressAndContinue() {
+    if (this.addressForm.invalid) return;
+
+    this.savingAddress = true;
+    this.accountService.updateAddress(this.addressForm.getRawValue()).subscribe({
+      next: () => {
+        this.savingAddress = false;
+        this.currentStep = 2;
+      },
+      error: () => {
+        this.savingAddress = false;
       }
     });
   }
